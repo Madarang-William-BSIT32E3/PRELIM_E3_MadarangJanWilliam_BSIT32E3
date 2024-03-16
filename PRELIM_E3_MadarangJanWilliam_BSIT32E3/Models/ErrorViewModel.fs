372da847f93a25f3267e8d50ac3ad48d6d0053e6
Liam <161627962+Madarang-William-BSIT32E3@users.noreply.github.com>
2024-03-16 14:06:19 +0800

    Delete PRELIM_E3_MadarangJanWilliam_BSIT32E3 directory

--- PRELIM_E3_MadarangJanWilliam_BSIT32E3/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace PRELIM_E3_MadarangJanWilliam_BSIT32E3.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
